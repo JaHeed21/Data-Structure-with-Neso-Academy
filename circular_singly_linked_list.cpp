6ac4aea3427f3d87142692b8c10d74b846925488
JaHeed21 <jahidulrayhan2002@gmail.com>
2022-10-25 17:12:23 +0600

    create function to add node at intermidiate position

--- circular_singly_linked_list.cpp
+++ circular_singly_linked_list.cpp
@@ -16,6 +16,40 @@ struct node *add_at_empty(struct node *tail, int data)
     tail=temp;
     return tail;
 }
+//adding nodes at the beginning of the list
+void add_at_beg(struct node *tail, int data)
+{
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    temp->data=data;
+    temp->next=temp;
+
+    temp->next=tail->next;
+    tail->next=temp;
+}
+
+//adding nodes at intermediate position
+struct node *add_at_pos(struct node *tail, int data, int pos)
+{
+    struct node *temp= (struct node*)malloc(sizeof(struct node));
+    temp->data=data;
+    temp->next=temp;
+
+    struct node *head=tail->next;
+    while(pos>2)
+    {
+        head=head->next;
+        pos--;
+    }
+    temp->next=head->next;
+    head->next=temp;
+    if(head==tail)
+    {
+        tail=temp;
+        return tail;
+    }
+    return tail;
+}
+
 //adding next and so on nodes on the list
 struct node *add_at_end(struct node *tail, int data)
 {
@@ -47,6 +81,11 @@ int main()
 
     tail=add_at_end(tail,55);
     tail=add_at_end(tail,75);
+
+    add_at_beg(tail,25);
+
+    tail=add_at_pos(tail,45,3);
+    tail=add_at_pos(tail,65,5);
 
     print_list(tail);
 
