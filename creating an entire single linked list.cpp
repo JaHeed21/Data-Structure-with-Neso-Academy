79a26641cafaacc426e7271642ee04cdf9a025a1
JaHeed21 <90699754+JaHeed21@users.noreply.github.com>
2022-11-05 14:48:31 +0600

    Adding function for counting the nodes of the list

--- creating an entire single linked list.cpp
+++ creating an entire single linked list.cpp
@@ -125,6 +125,23 @@ struct node* del_at_pos(struct node *head, int pos)
     }
 }
 
+void count_node(struct node* head)
+{
+    if(head==NULL)
+    {
+        cout<<"There is no node in the list"<<endl;
+    }
+    int cnt=1;
+    while(head->next!=NULL)
+    {
+        head=head->next;
+        cnt++;
+    }
+    cout<<"There are "<< cnt <<" nodes in the list"<<endl;
+}
+
+
+
 //printing entire list
 void print_list(struct node *head)
 {
@@ -148,21 +165,25 @@ int main()
     add_at_end(head,50);
     add_at_end(head,60);
     add_at_end(head,70);
+    
+    
 
     head=delFirst(head);
 
     delLast(head);
 
     print_list(head);
+    cout<<endl;
+    
+    count_node(head);
+    
 /*
     cout<< "Before deletion: ";
     add_at_pos(head,30,2);
     print_list(head);
     cout<<endl;
-
     cout<< "After Deletion: ";
     head=del_at_pos(head, 1);
     print_list(head);
-
 */
 }
